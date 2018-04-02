7d729596db76fd354dfc742cf8d86f0d56e8bcdd
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-02 21:24:24 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@ def countdown(number)
     puts "#{number} SECOND(S)!"
     number -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
@@ -13,5 +13,5 @@ def countdown_with_sleep(number)
     number -= 1
   end
   sleep 1
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 end
